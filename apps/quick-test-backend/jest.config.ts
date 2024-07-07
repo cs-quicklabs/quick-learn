7debdda4d832ea5e95d77062b7d12a34eb1430cc
Shiv Raj Bhagat <shivraj.bhagat@crownstack.com>
2024-07-07 22:55:41 +0530

    Added backend to the project

---
+++ apps/quick-test-backend/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'quick-test-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/quick-test-backend',
+};
