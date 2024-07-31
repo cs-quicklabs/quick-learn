36e4bfa691cdd3e2b17e0a1b33f88571211156fd
Bhupesh Kathuria <bhupesh.kathuria@crownstack.com>
2024-07-31 12:48:47 +0530

    added logout route & logout feature

--- apps/quick-learn-frontend/src/app/dashboard/page.tsx
+++ apps/quick-learn-frontend/src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {/* Your dashboard content here */}
-    </div>
+    <>
+      <div>
+        <h1>Dashboard</h1>
+        {/* Your dashboard content here */}
+      </div>{' '}
+    </>
   );
 };
 
