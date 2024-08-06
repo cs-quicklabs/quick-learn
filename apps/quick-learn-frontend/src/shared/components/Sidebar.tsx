27a1f28f6d9c891cea8f14e6c31938e96bb694a1
Bhupesh Kathuria <bhupesh.kathuria@crownstack.com>
2024-08-06 12:12:01 +0530

    heroicons updated in sidebar

--- apps/quick-learn-frontend/src/shared/components/Sidebar.tsx
+++ apps/quick-learn-frontend/src/shared/components/Sidebar.tsx
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 import React from 'react';
+import {
+  ClipboardDocumentListIcon,
+  LockClosedIcon,
+  TagIcon,
+  AcademicCapIcon,
+} from '@heroicons/react/20/solid';
 
 const Sidebar = () => {
   return (
@@ -9,92 +15,37 @@ const Sidebar = () => {
           href="/quick-learn/settings/team/general"
           className="bg-gray-200 text-gray-900 hover:bg-gray-200 px-3 py-2 flex items-center text-sm font-medium"
         >
-          <svg
-            className="w-6 h-6 text-gray-800 dark:text-white"
+          <ClipboardDocumentListIcon
             aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M15 9h3m-3 3h3m-3 3h3m-6 1c-.3-.6-1-1-1.6-1H7.6c-.7 0-1.3.4-1.6 1M4 5h16c.6 0 1 .4 1 1v12c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1V6c0-.6.4-1 1-1Zm7 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
-            ></path>
-          </svg>
+            className="h-5 w-5 text-black-400"
+          />
           <span className="truncate ml-2">General</span>
         </Link>
         <Link
           href="/quick-learn/settings/team/skills"
           className="text-gray-900 hover:text-gray-900 hover:bg-gray-50 px-3 py-2 flex items-center text-sm font-medium"
         >
-          <svg
-            className="w-6 h-6 text-gray-800 dark:text-white"
+          <LockClosedIcon
             aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14v3m-3-6V7a3 3 0 1 1 6 0v4m-8 0h10c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H7a1 1 0 0 1-1-1v-7c0-.6.4-1 1-1Z"
-            ></path>
-          </svg>
+            className="h-5 w-5 text-black-400"
+          />
           <span className="truncate ml-2">Primary Skills</span>
         </Link>
         <Link
           href="/quick-learn/settings/team/roadmap-categories"
           className="text-gray-900 hover:text-gray-900 hover:bg-gray-50 px-3 py-2 flex items-center text-sm font-medium"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-gray-800 dark:text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 6h.008v.008H6V6Z"
-            ></path>
-          </svg>
+          <TagIcon aria-hidden="true" className="h-5 w-5 text-black-400" />
           <span className="truncate ml-2">Roadmap Categories</span>
         </Link>
         <Link
           href="/quick-learn/settings/team/courses-categories"
           className="text-gray-900 hover:text-gray-900 hover:bg-gray-50 px-3 py-2 flex items-center text-sm font-medium"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-gray-800 dark:text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 6h.008v.008H6V6Z"
-            ></path>
-          </svg>
+          <AcademicCapIcon
+            aria-hidden="true"
+            className="h-5 w-5 text-black-400"
+          />
           <span className="truncate ml-2">Courses Categories</span>
         </Link>
       </nav>
