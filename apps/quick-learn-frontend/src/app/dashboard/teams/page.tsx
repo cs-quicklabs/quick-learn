f3364c86fafdcea70be7d5ecfcf024fff50146ab
Shiv Raj Bhagat <shivraj.bhagat@crownstack.com>
2024-08-12 12:25:13 +0530

    Enhance: added the id to the search text

--- apps/quick-learn-frontend/src/app/dashboard/teams/page.tsx
+++ apps/quick-learn-frontend/src/app/dashboard/teams/page.tsx
@@ -76,6 +76,7 @@ const TeamMemberListing = () => {
               className="bg-gray-50 w-64 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block"
               placeholder="Search Members"
               onChange={handleQueryChange}
+              id="search"
             />
             <Link
               href={`${RouteEnum.TEAM}/add`}
