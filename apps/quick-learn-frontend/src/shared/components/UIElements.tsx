3f8ae92e7ebfd98fda56f8bdab9b00d1016a7d06
Shiv Raj Bhagat <shivraj.bhagat@crownstack.com>
2024-08-27 22:32:43 +0530

    Minor ui fixes

--- apps/quick-learn-frontend/src/shared/components/UIElements.tsx
+++ apps/quick-learn-frontend/src/shared/components/UIElements.tsx
@@ -128,3 +128,23 @@ export const ShowInfoIcon = () => {
     </svg>
   );
 };
+
+export const CustomClipBoardIcon = ({ color }: { color?: string }) => {
+  return (
+    <svg
+      className="text-gray-800 h-3.5 w-3.5 mr-1"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill={color || ''}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fillRule="evenodd"
+        d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-6 8a1 1 0 0 1 1-1h6a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
