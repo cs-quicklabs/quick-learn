8ff4d726cdb8db715d4caf72d6ad5a78f005837e
snehdeep27 <>
2024-10-07 12:00:42 +0530

    initialisation

---
+++ apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/archived/roadmaps/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'Roadmap Categories • Quick Learn',
+  description: 'Roadmap Categories quick learn',
+};
+
+const page = () => {
+  return <div>roadmaps</div>;
+};
+
+export default page;
---
+++ apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/archived/users/ArchivedUsers.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ArchivedUsers = () => {
+  return <div>ArchivedUsers</div>;
+};
+
+export default ArchivedUsers;
