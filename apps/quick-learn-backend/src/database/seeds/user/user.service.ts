94c76fc31573f81e2418b23cac4b53aea9c293ac
Shiv Raj Bhagat <rajshiv169@gmail.com>
2024-10-17 18:56:39 +0530

    Fix: Seeding fixes for the first seed

--- apps/quick-learn-backend/src/database/seeds/user/user.service.ts
+++ apps/quick-learn-backend/src/database/seeds/user/user.service.ts
@@ -5,6 +5,7 @@ import { UserEntity } from '@src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { TeamService } from '../team/team.service';
 import { SkillService } from '../skill/skill.service';
+import { SkillEntity } from '@src/entities';
 
 @Injectable()
 export class UserService {
@@ -13,19 +14,22 @@ export class UserService {
     private readonly repository: Repository<UserEntity>,
     private readonly teamService: TeamService,
     private readonly skillService: SkillService,
-  ) {}
+  ) { }
 
   async run() {
     const team = await this.teamService.getTeam();
     const superAdmin = await this.repository.count({
       where: { user_type_id: UserTypeIdEnum.SUPERADMIN },
     });
-    let skill = await this.skillService.get();
-    if (!skill) {
+    const skills = await this.skillService.getMany();
+    let skill: SkillEntity;
+    if (skills.length === 0) {
       skill = await this.skillService.create({
         name: 'Crownstack Test',
         team_id: team.id,
       });
+    } else {
+      skill = skills[0];
     }
     if (!superAdmin) {
       const user: Partial<UserEntity> = {
