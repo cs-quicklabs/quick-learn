99fc363b858efa1f94ba9a032c48296ffa5b996b
snehdeep27 <singhsnehdeep99@gmail.com>
2024-10-25 12:18:51 +0530

    adding class validator to update-roadmap.dto

--- apps/quick-learn-backend/src/routes/roadmap/dto/update-roadmap.dto.ts
+++ apps/quick-learn-backend/src/routes/roadmap/dto/update-roadmap.dto.ts
@@ -1,6 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateRoadmapDto } from './create-roadmap.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateRoadmapDto extends PartialType(CreateRoadmapDto) {
+  @ApiProperty({
+    description: 'Active status of the roadmap',
+    example: true,
+    required: false,
+  })
+  @IsBoolean()
+  @IsOptional()
   active?: boolean;
 }
