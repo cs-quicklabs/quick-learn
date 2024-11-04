3e65c413e9802a71616a0d2499d6c0c6e52708e2
snehdeep27 <singhsnehdeep99@gmail.com>
2024-11-04 12:52:43 +0530

    Replaced custom lesson card implementation with reusable Card component

--- apps/quick-learn-frontend/src/shared/components/Card.tsx
+++ apps/quick-learn-frontend/src/shared/components/Card.tsx
@@ -7,31 +7,40 @@ interface CardProps {
   stats?: string;
   id: string;
   link?: string;
+  className?: string; // Add this line
 }
-
-const Card: FC<CardProps> = ({ id, title, description, stats, link = '#' }) => {
+const Card: FC<CardProps> = ({
+  id,
+  title,
+  description,
+  stats,
+  link = '#',
+  className = '',
+}) => {
   return (
     <Link
       id={id}
       href={link}
-      className="inline-block col-span-1 rounded-lg bg-white shadow-sm hover:shadow-lg border-gray-100 group w-full"
+      className={`inline-block col-span-1 rounded-lg shadow-sm hover:shadow-lg border-gray-100 group w-full ${className}`}
     >
-      <div className="flex-wrap py-4 px-6 text-gray-900">
-        <h1
-          id="message-heading"
-          className="font-medium text-gray-900 line-clamp-2 h-[48px] group-hover:underline capitalize"
-        >
-          {title}
-        </h1>
-        <p className="font-normal text-sm text-gray-500 line-clamp-3 h-[60px] mt-2">
-          {description}
-        </p>
+      <div className="flex flex-col h-full py-4 px-6 text-gray-900">
+        <div>
+          <h1
+            id="message-heading"
+            className="font-medium text-gray-900 line-clamp-2 h-[48px] group-hover:underline capitalize"
+          >
+            {title}
+          </h1>
+          <p className="font-normal text-sm text-gray-500 line-clamp-3 h-[60px] mt-2">
+            {description}
+          </p>
+        </div>
         {stats ? (
-          <p className="font-normal text-xs text-gray-500 line-clamp-2 mt-4 pb-2 capitalize">
+          <p className="font-normal text-xs text-gray-500 line-clamp-2 mt-auto pt-4 capitalize">
             {stats}
           </p>
         ) : (
-          <div className="h-[42px]" />
+          <div className="mt-auto" />
         )}
       </div>
     </Link>
