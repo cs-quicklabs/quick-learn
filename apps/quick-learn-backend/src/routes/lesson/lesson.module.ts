e6fb8605aec54c80e596a07c4f1590584582bd8e
Shiv Raj Bhagat <shivraj.bhagat@crownstack.com>
2024-11-26 19:33:05 +0530

    Revert "Merge pull request #179 from cs-quicklabs/revert-176-main"
    This reverts commit 1a76499bf7191f303101d35ec9810c097f037d4a, reversing
    changes made to 3f436859b2af80b80b7e0147dbd51b2dd1c5ccc5.

--- apps/quick-learn-backend/src/routes/lesson/lesson.module.ts
+++ apps/quick-learn-backend/src/routes/lesson/lesson.module.ts
@@ -4,9 +4,10 @@ import { LessonEntity } from '@src/entities';
 import { LessonController } from './lesson.controller';
 import { LessonService } from './lesson.service';
 import { CourseModule } from '../course/course.module';
+import { FileModule } from '@src/file/file.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LessonEntity]), CourseModule],
+  imports: [TypeOrmModule.forFeature([LessonEntity]), CourseModule , FileModule],
   providers: [LessonService],
   controllers: [LessonController],
   exports: [LessonService],
