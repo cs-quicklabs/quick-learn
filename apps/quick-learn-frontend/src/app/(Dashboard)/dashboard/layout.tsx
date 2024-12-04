de1d62242df912d730f67f0fd376241ea1f1bec5
snehdeep27 <singhsnehdeep99@gmail.com>
2024-12-04 11:01:26 +0530

    Metadata fetching fix

--- apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/layout.tsx
+++ apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/layout.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { UserProvider } from '@src/context/userContext';
 import Navbar from '@src/shared/components/Navbar';
-import { fetchMetadata } from '@src/store/features/metadataSlice';
 import { selectHideNavbar } from '@src/store/features/uiSlice';
-import { useAppDispatch, useAppSelector } from '@src/store/hooks';
+import { useAppSelector } from '@src/store/hooks';
 import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function Layout({
   children,
@@ -14,7 +12,6 @@ export default function Layout({
 }) {
   const hideNavbar = useAppSelector(selectHideNavbar);
   const pathname = usePathname();
-  const dispatch = useAppDispatch();
 
   // Add paths that should be full width
   const fullWidthPaths = ['/dashboard/learning-path'];
@@ -25,10 +22,6 @@ export default function Layout({
     ? 'mt-16 w-full'
     : 'max-w-screen-2xl mx-auto mt-16 py-3 px-4 sm:py-5 lg:px-8';
 
-  useEffect(() => {
-    dispatch(fetchMetadata());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return (
     <UserProvider>
       <div className="w-full">
