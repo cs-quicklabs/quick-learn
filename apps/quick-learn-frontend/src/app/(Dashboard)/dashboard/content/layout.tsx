e6df12d662b1c2953616e649a99e0570ea1a2f0f
snehdeep27 <singhsnehdeep99@gmail.com>
2024-12-13 13:22:58 +0530

    removing dispatch from layout

--- apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/content/layout.tsx
+++ apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/content/layout.tsx
@@ -1,25 +1,9 @@
 'use client';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  fetchMetadata,
-  selectIsMetadataInitialized,
-} from '@src/store/features/metadataSlice';
-import { AppDispatch } from '@src/store/store';
 
 export default function Layout({
   children,
 }: {
   readonly children: React.ReactNode;
 }) {
-  const dispatch = useDispatch<AppDispatch>();
-  const isInitialized = useSelector(selectIsMetadataInitialized);
-
-  useEffect(() => {
-    if (!isInitialized) {
-      dispatch(fetchMetadata());
-    }
-  }, [dispatch, isInitialized]);
-
   return <>{children}</>;
 }
