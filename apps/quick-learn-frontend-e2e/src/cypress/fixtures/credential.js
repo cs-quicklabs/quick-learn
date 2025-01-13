1819aeae91ddad663601c3bffe5b782e20055899
AnishkaR-byte <anishka.rai1@crownstack.com>
2025-01-13 12:08:14 +0530

    Added Change Password script

--- apps/quick-learn-frontend-e2e/src/cypress/fixtures/credential.js
+++ apps/quick-learn-frontend-e2e/src/cypress/fixtures/credential.js
@@ -2,3 +2,8 @@ export const validCredentials = {
   mail: 'super.admin@yopmail.com',
   password: 'password@123P',
 };
+
+export const AdminValidCredentials = {
+  mail: 'user11945@yopmail.com',
+  password: 'Password@123',
+};
