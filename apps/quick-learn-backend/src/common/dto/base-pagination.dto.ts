80ca242c14739c168578e7680bc6bdaa540bdd23
Gagan-Crownstack <gagan@crownstack.com>
2025-01-15 13:27:23 +0530

    Fix: LEARN-40 In the Archive section, searching for a user and pressing Enter reloads the entire page and Sorted is last deactivated (#278)
    Fix for archive section re-rendering on pressing enter also data is SORTED

--- apps/quick-learn-backend/src/common/dto/base-pagination.dto.ts
+++ apps/quick-learn-backend/src/common/dto/base-pagination.dto.ts
@@ -3,21 +3,21 @@ import { Type } from 'class-transformer';
 import { IsIn, IsInt, IsOptional, IsString, Min } from 'class-validator';
 
 export class BasePaginationDto {
-  @ApiProperty({ example: 1 })
+  @ApiProperty({ example: 1, required: false })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
   page?: number = 1;
 
-  @ApiProperty({ example: 10 })
+  @ApiProperty({ example: 10, required: false })
   @IsOptional()
   @IsInt()
   @IsIn([10, 25, 50, 100])
   @Type(() => Number)
   limit?: number = 10;
 
-  @ApiProperty({ example: 'paginate' })
+  @ApiProperty({ example: 'q', required: false })
   @IsOptional()
   @IsString()
   q?: string;
