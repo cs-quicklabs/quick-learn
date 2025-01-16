97e431b3287edf3511a61b074aeab15666e31a19
Ujjwal164 <ujjwalsaini164@gmail.com>
2025-01-16 17:34:53 +0530

    LEARN50

--- apps/quick-learn-frontend/src/constants/lang/en.ts
+++ apps/quick-learn-frontend/src/constants/lang/en.ts
@@ -433,6 +433,7 @@ export const en = {
     openMenu: 'Open main menu',
     viewNotification: 'View notifications',
     profile: 'My Profile',
+    accountSetting:'Account Setting',
     signOut: 'Sign out',
 
     //Search Box
