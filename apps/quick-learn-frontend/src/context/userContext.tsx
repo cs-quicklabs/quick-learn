3af60781c9312e77ee62704689a3a05e305900ef
Gagan-Crownstack <gagan@crownstack.com>
2025-01-17 14:12:11 +0530

    refactored categories not showing

--- apps/quick-learn-frontend/src/context/userContext.tsx
+++ apps/quick-learn-frontend/src/context/userContext.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { getUser } from '@src/apiServices/authService';
+import { getContentRepositoryMetadata } from '@src/apiServices/contentRepositoryService';
 import { TUser } from '@src/shared/types/userTypes';
+import { updateContentRepository } from '@src/store/features/metadataSlice';
+import { useAppDispatch } from '@src/store/hooks';
 import { showApiErrorInToast } from '@src/utils/toastUtils';
+import { UserTypeIdEnum } from 'lib/shared/src';
 import { createContext, useState, useEffect, useMemo } from 'react';
 
 export const UserContext = createContext<{
@@ -16,11 +20,24 @@ export const UserContext = createContext<{
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<TUser | null>(null);
+  const dispatch = useAppDispatch();
+
+  const fetchContentRepositoryMetadata = async () => {
+    try {
+      const res = await getContentRepositoryMetadata();
+      dispatch(updateContentRepository(res.data)); //update redux store metadata
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     getUser()
       .then((res) => {
         setUser(res.data);
+        if (res.data.user_type_id != UserTypeIdEnum.MEMBER) {
+          fetchContentRepositoryMetadata();
+        }
       })
       .catch((err) => showApiErrorInToast(err));
   }, []);
