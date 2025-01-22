c8686e9d46ccad7778e966254c71cdf030c10a2f
Gagan-Crownstack <gagan@crownstack.com>
2025-01-22 17:07:12 +0530

    seperated the logic from userContext

--- apps/quick-learn-frontend/src/context/userContext.tsx
+++ apps/quick-learn-frontend/src/context/userContext.tsx
@@ -1,13 +1,9 @@
 'use client';
 import { getUser } from '@src/apiServices/authService';
-import { getContentRepositoryMetadata } from '@src/apiServices/contentRepositoryService';
 import { TUser } from '@src/shared/types/userTypes';
-import { updateContentRepository } from '@src/store/features/metadataSlice';
-import { useAppDispatch } from '@src/store/hooks';
 import { showApiErrorInToast } from '@src/utils/toastUtils';
-import { UserTypeIdEnum } from 'lib/shared/src';
-import { usePathname } from 'next/navigation';
 import { createContext, useState, useEffect, useMemo } from 'react';
+import { useFetchContentRepositoryMetadata } from './contextHelperService';
 
 export const UserContext = createContext<{
   user: TUser | null;
@@ -21,30 +17,13 @@ export const UserContext = createContext<{
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<TUser | null>(null);
-  const path = usePathname();
-  const dispatch = useAppDispatch();
-
-  const fetchContentRepositoryMetadata = async () => {
-    try {
-      const res = await getContentRepositoryMetadata();
-      dispatch(updateContentRepository(res.data)); //update redux store metadata
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const fetchMetadata = useFetchContentRepositoryMetadata();
 
   useEffect(() => {
     getUser()
       .then((res) => {
         setUser(res.data);
-        console.log(path);
-
-        if (
-          path !== '/dashboard/content' &&
-          res.data.user_type_id != UserTypeIdEnum.MEMBER
-        ) {
-          fetchContentRepositoryMetadata();
-        }
+        fetchMetadata(res.data.user_type_id);
       })
       .catch((err) => showApiErrorInToast(err));
     // eslint-disable-next-line react-hooks/exhaustive-deps
