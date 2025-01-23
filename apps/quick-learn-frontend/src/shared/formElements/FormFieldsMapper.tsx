5ee2bc9313e37ac07618ad296c75f7a4ad5a2aca
Ujjwal164 <ujjwalsaini164@gmail.com>
2025-01-23 13:31:11 +0530

    fixes

--- apps/quick-learn-frontend/src/shared/formElements/FormFieldsMapper.tsx
+++ apps/quick-learn-frontend/src/shared/formElements/FormFieldsMapper.tsx
@@ -62,12 +62,12 @@ function FormFieldsMapper<T extends z.ZodTypeAny>({
     reset,
     formState: { errors, isValid, isDirty },
   } = methods || defaultMethods;
-  const [refreshKey, setRefreshKey] = useState(0);
+  const [formReset, setFormReset] = useState(0);
   const handleFormSubmit = (data: z.infer<T>) => {
     onSubmit(data, reset);
     if (resetFormOnSubmit) {
       reset();
-      setRefreshKey((prevKey) => prevKey + 1);
+      setFormReset((prevKey) => prevKey + 1);
     }
   };
 
@@ -99,7 +99,7 @@ function FormFieldsMapper<T extends z.ZodTypeAny>({
       <form
         onSubmit={handleSubmit(handleFormSubmit)}
         className="space-y-4"
-        key={refreshKey}
+        key={formReset}
         noValidate
       >
         {fields.map((field) => {
