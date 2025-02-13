0f06b749e3c052bc7f23f34eb19cae0e279de7ab
Gagan-Crownstack <gagan@crownstack.com>
2025-02-13 19:20:37 +0530

    fixed swagger problem

--- apps/quick-learn-backend/src/routes/leaderboard/dto/leaderboard-query.dto.ts
+++ apps/quick-learn-backend/src/routes/leaderboard/dto/leaderboard-query.dto.ts
@@ -2,13 +2,14 @@ import { IsEnum } from 'class-validator';
 import { LeaderboardTypeEnum } from '@src/common/constants/constants';
 import { BasePaginationDto } from '@src/common/dto';
 import { ApiProperty } from '@nestjs/swagger';
+
 export class LeaderboardQueryDto extends BasePaginationDto {
   @ApiProperty({
-    name: 'type',
-    required: true,
-    type: LeaderboardTypeEnum,
+    enum: LeaderboardTypeEnum,
+    enumName: 'LeaderboardTypeEnum',
     description: 'Get what is the type of the leaderboard',
     default: LeaderboardTypeEnum.WEEKLY,
+    required: true,
   })
   @IsEnum(LeaderboardTypeEnum)
   type: LeaderboardTypeEnum;
