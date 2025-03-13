2e343363e9491912c2d0563267d8998fe2de9707
Gagan-Crownstack <gagan@crownstack.com>
2025-03-13 14:04:37 +0530

    added metadata

--- apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/archived/lessons/[lesson]/page.tsx
+++ apps/quick-learn-frontend/src/app/(Dashboard)/dashboard/archived/lessons/[lesson]/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ViewArchivedLesson from './ViewArchivedLesson';
 
+export const metadata = {
+  title: 'Archived Lesson • Quick Learn',
+  description: 'Archived Lesson quick learn',
+};
+
 const page = () => {
   return (
     <div>
